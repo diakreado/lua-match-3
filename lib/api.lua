da5d4bf8c87a8afa50f48b0e1b0011826bdf589c
mikle9997 <mikle9997@mail.ru>
2019-09-14 22:04:47 +0300

    finding 3 match

--- lib/api.lua
+++ lib/api.lua
@@ -5,11 +5,60 @@ local lib = {
   field = {}
 }
 
+local function getRandomLetter()
+  letterArray = {'A','B','C','D','E','F'}
+  math.randomseed(os.clock()*1000000000000)
+
+  local i = math.random(#letterArray)
+  return letterArray[i]
+end
+
+function lib.checkFields()
+  local lines = {}
+
+  -- check horizontal line
+  for i = 1, lib.n do
+    local j = 1
+    while j <= lib.m do
+      if lib.field[i][j] == lib.field[i][j + 1] and lib.field[i][j] == lib.field[i][j + 2] then
+        local k = j + 1
+        while lib.field[i][j] == lib.field[i][k] do
+          k = k + 1
+        end
+        lines[#lines + 1] = { pos = 'h', i = i, j = j, k = k-j}
+        j = k
+      end 
+      j = j + 1
+    end
+  end
+
+  -- check vertical line
+  for i = 1, lib.m do
+    local j = 1
+    while j <= lib.n do
+      if lib.field[j + 1] ~= nil and 
+         lib.field[j][i] == lib.field[j + 1][i] and
+         lib.field[j + 2] ~= nil and 
+         lib.field[j][i] == lib.field[j + 2][i] then
+        local k = j + 1
+        while lib.field[k] ~= nil and lib.field[j][i] == lib.field[k][i] do
+          k = k + 1
+        end
+        lines[#lines + 1] = {pos = 'v', j = j, i = i, k = k-j}
+        j = k
+      end 
+      j = j + 1
+    end
+  end
+
+  return lines
+end
+
 function lib.init()
   for i= 1, lib.n do
     lib.field[i] = {}
     for j= 1, lib.m do
-      lib.field[i][j] = 'A'
+      lib.field[i][j] = getRandomLetter()
     end
   end
 end
--- main.lua
+++ main.lua
@@ -4,17 +4,17 @@ lib.init()
 
 function drawField(field)
   io.write('   ')
-  for i = 1, #field do
+  for i = 1, #field[1] do
     io.write(i - 1 .. ' ')
   end
   io.write('\n  ')
-  for i = 1, #field do
+  for i = 1, #field[1] do
     io.write('--')
   end
   io.write('\n')
-  for i = 1, 10 do
+  for i = 1, #field do
     io.write(i - 1 .. ' | ')
-    for j = 1, 10 do
+    for j = 1, #field[i] do
       io.write(field[i][j] .. ' ')
     end
     io.write('\n')
@@ -22,3 +22,12 @@ function drawField(field)
 end
 
 drawField(lib.dump())
+
+print()
+
+lines = lib.checkFields()
+
+for i=1,#lines do
+  print(lines[i].pos, lines[i].i, lines[i].j, lines[i].k)
+end
+
