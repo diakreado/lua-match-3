16eb92c6758cc47e14ebe5f4031cab0dfeb6fe43
mikle9997 <mikle9997@mail.ru>
2019-09-15 14:17:57 +0300

    changed test and added text to ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # lua-match-3
+
+## Dev-dependences:
+
+| Name | Links |
+| ------ | ----------- |
+| LuaUnit   | [Github](https://github.com/bluebird75/luaunit), [LuaRocks](https://luarocks.org/modules/bluebird75/luaunit), [ReadTheDocs](https://luaunit.readthedocs.io/en/latest/) |
--- test.lua
+++ test.lua
@@ -1,25 +1,21 @@
 lu = require('luaunit')
 findMatch = require('game-logic/findMatch')
 
+TestFindMatch = {} --class
 
-TestSucFind = {} --class
-  TestSucFind.field = {
-    {'A','B','C','D'},
-    {'D','C','C','C'},
-    {'A','B','C','D'},
-    {'D','C','C','A'}
-  }
-
-  function TestSucFind:testExample()
-    lu.assertEquals( findMatch(TestSucFind.field), {
+  function TestFindMatch:testExample()
+    local field = {
+      {'A','B','C','D'},
+      {'D','C','C','C'},
+      {'A','B','C','D'},
+      {'D','C','C','A'}
+    }
+    lu.assertEquals( findMatch(field), {
       {orientation = 'h', x = 2, y = 2, len = 3},
       {orientation = 'v', x = 3, y = 1, len = 4}
     })
   end
   
-  function TestSucFind:testTestOfTestOfTest()
-    lu.assertEquals( 5*2, 10)
-  end
--- class TestSucFind
+-- class TestFindMatch
 
 os.exit( lu.LuaUnit.run() )
